2ee34d99e8a09da9397ccd318b0207cb71effc8d
bmp713 <bmp713@yahoo.com>
2022-07-10 17:47:28 -0700

    heroku

--- server.js
+++ server.js
@@ -3,13 +3,14 @@ const cors = require('cors');
 const express = require('express');
 const bodyParser = require('body-parser');
 const axios = require("axios");
+const port = process.env.PORT || 5000; 
 const app = express(); 
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 
-app.listen(process.env.PORT || 5000, () => { 
+app.listen(port, () => { 
     console.log('Server listening on port', port) 
 });
 
